e5a64aabbcaa4b36445602bae32789dcd35bb057
Dmitry Kovalchuk <55485703+privettalon@users.noreply.github.com>
2022-06-07 06:50:19 +0300

    православний коміт

--- src/main/java/enums/ProductItem.java
+++ src/main/java/enums/ProductItem.java
@@ -6,8 +6,9 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ProductItem {
 
-    DRESS_ISSA_PLUS("C&A біла кежуал сукня однотонна — виробництво Німеччина", "Сукня C&A", "md autlet"),
-    WHITE_NENKA_DRESS("C&A футболка однотонна світло-рожева Кежуал виробництво — Німеччина","Футболка C&A","md autlet");
+    DRESS_ISSA_PLUS("Garne сіра сукня karen однотонна — виробництво Україна", "Сукня KAREN Garne", "Garne"),
+    WHITE_NENKA_DRESS("Garne комбінованя кежуал сукня ornella однотонна — виробництво Україна","Сукня ORNELLA Garne","Garne");
+
 
     @Getter
     private String name;
